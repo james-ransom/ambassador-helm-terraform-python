8f919601ad7b9bb216d3ba246f3145d01061469b
james ransom <ransom1538@protonmail.com>
2024-05-22 21:35:13 -0400

    Updates for faster testing, I am testing to move just to Envoy

--- app/build.sh
+++ app/build.sh
@@ -9,17 +9,29 @@ if [ ! -f $1 ]; then
     exit 0; 
 fi
 
+current_timestamp=$(date +%s)
+
 # Log INTO GCP 
 docker login -u _json_key -p "$(cat $1)" https://us-central1-docker.pkg.dev 
 gcloud auth configure-docker
-docker buildx build --platform=linux/amd64  --tag us-central1-docker.pkg.dev/$2/backend/ambassador-helm-tf:1.0 .
+docker buildx build --platform=linux/amd64  --tag us-central1-docker.pkg.dev/$2/backend/ambassador-helm-tf:$current_timestamp .
 
-
-docker tag us-central1-docker.pkg.dev/$2/backend/ambassador-helm-tf:1.0 us-central1-docker.pkg.dev/$2/backend/ambassador-helm-tf:latest
-docker push us-central1-docker.pkg.dev/$2/backend/ambassador-helm-tf:1.0
+docker tag us-central1-docker.pkg.dev/$2/backend/ambassador-helm-tf:$current_timestamp us-central1-docker.pkg.dev/$2/backend/ambassador-helm-tf:latest
+docker push us-central1-docker.pkg.dev/$2/backend/ambassador-helm-tf:$current_timestamp
 docker push us-central1-docker.pkg.dev/$2/backend/ambassador-helm-tf:latest
 
-#sed -i '' 's/VERSION/VER-1/g' main.py 
+sed -i '' "s|image: us-central1-docker.pkg.dev/$2/backend/ambassador-helm-tf:[0-9]*|image: us-central1-docker.pkg.dev/epic-jenkins/backend/ambassador-helm-tf:$current_timestamp|g" deployment.yaml
+sed -i '' "s|image: us-central1-docker.pkg.dev/$2/backend/ambassador-helm-tf:[0-9]*|image: us-central1-docker.pkg.dev/epic-jenkins/backend/ambassador-helm-tf:$current_timestamp|g" deployment-v2.yaml
+
+grep "image: us-central1-docker.pkg.dev/epic-jenkins/backend/ambassador-helm-tf:" deployment.yaml
+
+
+sed -i '' "s/\[VERSION_REPLACE\]/$current_timestamp/g" deployment.yaml
+sed -i '' "s/\[VERSION_REPLACE\]/$current_timestamp/g" deployment-v2.yaml
+
+sed -i '' "s/\[YOUR_PROJECTNAME_HERE\]/$2/g" deployment.yaml 
+sed -i '' "s/\[YOUR_PROJECTNAME_HERE\]/$2/g" deployment-v2.yaml
+
 
 kubectl apply -f deployment.yaml 
 kubectl apply -f deployment-v2.yaml
@@ -45,3 +57,8 @@ kubectl apply -f listen3.yaml
 kubectl apply -f host.yaml
 kubectl apply -f mapping-search.yaml
 kubectl apply -f mapping-groupings.yaml
+
+
+kubectl rollout restart deployment/backend-k8s-v2
+kubectl rollout restart deployment/backend-k8s
+
